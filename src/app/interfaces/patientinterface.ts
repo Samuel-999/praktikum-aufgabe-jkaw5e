050cd5a2974c9ea4938e49aac1c34168fec497b8
Samuel-999 <samuel.schwenn@icloud.com>
2021-06-10 15:15:56 +0200

    2 files modified, 2 added

---
+++ src/app/interfaces/patientinterface.ts
@@ -0,0 +1,12 @@
+export interface Patients{
+  id: number;
+  firstName: string;
+  lastName: string;
+  administrationDate: string;
+  birthday: string;
+  age: number;
+  hospital: string;
+  station: string;
+  room: string;
+  bed: number;
+}
--- src/app/patienten/patienten.component.ts
+++ src/app/patienten/patienten.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Patients } from '../interfaces/patientinterface';
+import { PatientListService } from '../patientmock.service';
 
 @Component({
   selector: 'app-patienten',
@@ -7,7 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PatientenComponent implements OnInit {
 
-  constructor() { }
+  patientData: Patients;
+
+
+  constructor(private patientservice: PatientListService) {
+    patientservice.getPatientInfo("1").subscribe(result => {this.patientData = result})
+  }
 
   ngOnInit() {
   }
