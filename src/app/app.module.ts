5d2a65444875d517d368c6dd84260d949d8145d8
Samuel-999 <samuel.schwenn@icloud.com>
2021-06-10 11:18:27 +0200

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,8 +9,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { FalleigenschaftenComponent } from './falleigenschaften/falleigenschaften.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
+import { faWrench } from '@fortawesome/free-solid-svg-icons/faWrench';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons/faCoffee';
 
-library.add()
+library.add(faWrench);
+library.add(faCoffee);
 
 @NgModule({
   imports: [BrowserModule, FormsModule, HttpClientModule, FontAwesomeModule],
