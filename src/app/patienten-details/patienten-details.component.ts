377ea201035948cfe82bcfc6073df8e653cf8138
Samuel-999 <samuel.schwenn@icloud.com>
2021-06-11 09:09:15 +0200

    6 files modified

--- src/app/patienten-details/patienten-details.component.ts
+++ src/app/patienten-details/patienten-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Patients } from '../interfaces/patientinterface';
+import { PatientListService } from '../patientmock.service';
 
 @Component({
   selector: 'app-patienten-details',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PatientenDetailsComponent implements OnInit {
 
-  constructor() { }
+   patientData: Patients;
+
+
+  constructor(private patientservice: PatientListService) {
+    patientservice.getPatientInfo("1").subscribe(result => {this.patientData = result})
+  }
 
   ngOnInit() {
   }
 
+}
+var coll = document.getElementsByClassName("more");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var normal = this.nextElementSibling;
+    if (normal.style.display === "block") {
+      normal.style.display = "none";
+    } else {
+      normal.style.display = "block";
+    }
+  });
 }
